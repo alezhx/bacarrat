a8a057753b6d8fc8e40eb898154b1ab47d0548ac
Alex H <alezhx@gmail.com>
2024-09-27 22:39:30 -0400

    chute logic

--- classes/chute.py
+++ classes/chute.py
@@ -1,18 +1,32 @@
-from playingcards import Card
+from playingcards import Card, Deck
+from classes import Shuffler
 
-# 6-8 decks
+# 6-8 deck_count
 class Chute:
-    def __init__(self, decks: int=8, ordered: bool=False):
-        self.__validate_params
-        self.ordered = ordered
+    def __init__(self, deck_count: int=8, shuffle_type: str="HAND", cut_position: int=-1):
+        self.chute = self.generate_chute(self, deck_count, shuffle_type)
+        self.cut_position = cut_position
+        # self.__validate_params(self.chute)
 
-    def generate_chute(self, decks: int, ordered: bool = False):
-        pass
+    def generate_chute(self, deck_count: int, shuffle_type: str="HAND"):
+        if shuffle_type == "HAND":
+            return [self.shuffle(Deck()) for _ in range(deck_count)]
+        if shuffle_type == "ELECTRONIC":
+            return self.shuffle([Deck() for _ in range(deck_count)])
+        
+        return [Deck() for _ in range(deck_count)]
+    
+    def shuffle(self, cards: list):
+        shuffler = Shuffler(cards=cards)
+        shuffler.shuffle()
+        return cards
+
+
 
     def remove_card(self, position):
-        self.cards.pop(position)
+        self.chute.pop(position)
 
-    def order_cards(self):
+    def order_chute_cards(self):
         if not self.ordered:
-            self.cards.sort()
+            self.chute_cards.sort()
             self.ordered = True
